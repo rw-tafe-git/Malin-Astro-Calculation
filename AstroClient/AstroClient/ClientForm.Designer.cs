4cd7e6822ec333680e588113f863ba356f76f77f
rw-tafe-git <124329775+rw-tafe-git@users.noreply.github.com>
2023-09-11 14:21:50 +0800

    Updated

--- AstroClient/AstroClient/ClientForm.Designer.cs
+++ AstroClient/AstroClient/ClientForm.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ClientForm));
             this.ListViewOutput = new System.Windows.Forms.ListView();
             this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
@@ -37,31 +38,39 @@
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.InputRest = new System.Windows.Forms.TextBox();
+            this.InputObserved = new System.Windows.Forms.TextBox();
+            this.ButtonCalculateVelocity = new System.Windows.Forms.Button();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.button2 = new System.Windows.Forms.Button();
+            this.InputParallax = new System.Windows.Forms.TextBox();
+            this.ButtonCalculateDistance = new System.Windows.Forms.Button();
             this.groupBox3 = new System.Windows.Forms.GroupBox();
             this.label4 = new System.Windows.Forms.Label();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.button3 = new System.Windows.Forms.Button();
+            this.InputCelsius = new System.Windows.Forms.TextBox();
+            this.ButtonCalculateTemperature = new System.Windows.Forms.Button();
             this.groupBox4 = new System.Windows.Forms.GroupBox();
-            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.InputMass = new System.Windows.Forms.NumericUpDown();
             this.label6 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
-            this.textBox5 = new System.Windows.Forms.TextBox();
-            this.button4 = new System.Windows.Forms.Button();
+            this.InputMass2 = new System.Windows.Forms.TextBox();
+            this.ButtonCalculateHorizon = new System.Windows.Forms.Button();
             this.ButtonGerman = new System.Windows.Forms.Button();
             this.ButtonFrench = new System.Windows.Forms.Button();
             this.ButtonEnglish = new System.Windows.Forms.Button();
+            this.MainMenu = new System.Windows.Forms.MainMenu(this.components);
+            this.menuItem1 = new System.Windows.Forms.MenuItem();
+            this.MenuBackgroundColour = new System.Windows.Forms.MenuItem();
+            this.MenuLight = new System.Windows.Forms.MenuItem();
+            this.MenuDark = new System.Windows.Forms.MenuItem();
+            this.MenuCustom = new System.Windows.Forms.MenuItem();
+            this.MenuButtonColour = new System.Windows.Forms.MenuItem();
+            this.MenuLabelColour = new System.Windows.Forms.MenuItem();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.groupBox3.SuspendLayout();
             this.groupBox4.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.InputMass)).BeginInit();
             this.SuspendLayout();
             // 
             // ListViewOutput
@@ -97,9 +106,9 @@
             // 
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Controls.Add(this.textBox2);
-            this.groupBox1.Controls.Add(this.textBox1);
-            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.InputRest);
+            this.groupBox1.Controls.Add(this.InputObserved);
+            this.groupBox1.Controls.Add(this.ButtonCalculateVelocity);
             resources.ApplyResources(this.groupBox1, "groupBox1");
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.TabStop = false;
@@ -114,27 +123,30 @@
             resources.ApplyResources(this.label1, "label1");
             this.label1.Name = "label1";
             // 
-            // textBox2
+            // InputRest
             // 
-            resources.ApplyResources(this.textBox2, "textBox2");
-            this.textBox2.Name = "textBox2";
+            resources.ApplyResources(this.InputRest, "InputRest");
+            this.InputRest.Name = "InputRest";
+            this.InputRest.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextBox_KeyPress);
             // 
-            // textBox1
+            // InputObserved
             // 
-            resources.ApplyResources(this.textBox1, "textBox1");
-            this.textBox1.Name = "textBox1";
+            resources.ApplyResources(this.InputObserved, "InputObserved");
+            this.InputObserved.Name = "InputObserved";
+            this.InputObserved.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextBox_KeyPress);
             // 
-            // button1
+            // ButtonCalculateVelocity
             // 
-            resources.ApplyResources(this.button1, "button1");
-            this.button1.Name = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            resources.ApplyResources(this.ButtonCalculateVelocity, "ButtonCalculateVelocity");
+            this.ButtonCalculateVelocity.Name = "ButtonCalculateVelocity";
+            this.ButtonCalculateVelocity.UseVisualStyleBackColor = true;
+            this.ButtonCalculateVelocity.Click += new System.EventHandler(this.ButtonCalculateVelocity_Click);
             // 
             // groupBox2
             // 
             this.groupBox2.Controls.Add(this.label3);
-            this.groupBox2.Controls.Add(this.textBox3);
-            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.InputParallax);
+            this.groupBox2.Controls.Add(this.ButtonCalculateDistance);
             resources.ApplyResources(this.groupBox2, "groupBox2");
             this.groupBox2.Name = "groupBox2";
             this.groupBox2.TabStop = false;
@@ -144,22 +156,24 @@
             resources.ApplyResources(this.label3, "label3");
             this.label3.Name = "label3";
             // 
-            // textBox3
+            // InputParallax
             // 
-            resources.ApplyResources(this.textBox3, "textBox3");
-            this.textBox3.Name = "textBox3";
+            resources.ApplyResources(this.InputParallax, "InputParallax");
+            this.InputParallax.Name = "InputParallax";
+            this.InputParallax.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextBox_KeyPress);
             // 
-            // button2
+            // ButtonCalculateDistance
             // 
-            resources.ApplyResources(this.button2, "button2");
-            this.button2.Name = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            resources.ApplyResources(this.ButtonCalculateDistance, "ButtonCalculateDistance");
+            this.ButtonCalculateDistance.Name = "ButtonCalculateDistance";
+            this.ButtonCalculateDistance.UseVisualStyleBackColor = true;
+            this.ButtonCalculateDistance.Click += new System.EventHandler(this.ButtonCalculateDistance_Click);
             // 
             // groupBox3
             // 
             this.groupBox3.Controls.Add(this.label4);
-            this.groupBox3.Controls.Add(this.textBox4);
-            this.groupBox3.Controls.Add(this.button3);
+            this.groupBox3.Controls.Add(this.InputCelsius);
+            this.groupBox3.Controls.Add(this.ButtonCalculateTemperature);
             resources.ApplyResources(this.groupBox3, "groupBox3");
             this.groupBox3.Name = "groupBox3";
             this.groupBox3.TabStop = false;
@@ -169,32 +183,35 @@
             resources.ApplyResources(this.label4, "label4");
             this.label4.Name = "label4";
             // 
-            // textBox4
+            // InputCelsius
             // 
-            resources.ApplyResources(this.textBox4, "textBox4");
-            this.textBox4.Name = "textBox4";
+            resources.ApplyResources(this.InputCelsius, "InputCelsius");
+            this.InputCelsius.Name = "InputCelsius";
+            this.InputCelsius.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextBox_KeyPress);
             // 
-            // button3
+            // ButtonCalculateTemperature
             // 
-            resources.ApplyResources(this.button3, "button3");
-            this.button3.Name = "button3";
-            this.button3.UseVisualStyleBackColor = true;
+            resources.ApplyResources(this.ButtonCalculateTemperature, "ButtonCalculateTemperature");
+            this.ButtonCalculateTemperature.Name = "ButtonCalculateTemperature";
+            this.ButtonCalculateTemperature.UseVisualStyleBackColor = true;
+            this.ButtonCalculateTemperature.Click += new System.EventHandler(this.ButtonCalculateTemperature_Click);
             // 
             // groupBox4
             // 
-            this.groupBox4.Controls.Add(this.numericUpDown1);
+            this.groupBox4.Controls.Add(this.InputMass);
             this.groupBox4.Controls.Add(this.label6);
             this.groupBox4.Controls.Add(this.label5);
-            this.groupBox4.Controls.Add(this.textBox5);
-            this.groupBox4.Controls.Add(this.button4);
+            this.groupBox4.Controls.Add(this.InputMass2);
+            this.groupBox4.Controls.Add(this.ButtonCalculateHorizon);
             resources.ApplyResources(this.groupBox4, "groupBox4");
             this.groupBox4.Name = "groupBox4";
             this.groupBox4.TabStop = false;
             // 
-            // numericUpDown1
+            // InputMass
             // 
-            resources.ApplyResources(this.numericUpDown1, "numericUpDown1");
-            this.numericUpDown1.Name = "numericUpDown1";
+            resources.ApplyResources(this.InputMass, "InputMass");
+            this.InputMass.Name = "InputMass";
+            this.InputMass.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextBox_KeyPress);
             // 
             // label6
             // 
@@ -206,16 +223,18 @@
             resources.ApplyResources(this.label5, "label5");
             this.label5.Name = "label5";
             // 
-            // textBox5
+            // InputMass2
             // 
-            resources.ApplyResources(this.textBox5, "textBox5");
-            this.textBox5.Name = "textBox5";
+            resources.ApplyResources(this.InputMass2, "InputMass2");
+            this.InputMass2.Name = "InputMass2";
+            this.InputMass2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextBox_KeyPress);
             // 
-            // button4
+            // ButtonCalculateHorizon
             // 
-            resources.ApplyResources(this.button4, "button4");
-            this.button4.Name = "button4";
-            this.button4.UseVisualStyleBackColor = true;
+            resources.ApplyResources(this.ButtonCalculateHorizon, "ButtonCalculateHorizon");
+            this.ButtonCalculateHorizon.Name = "ButtonCalculateHorizon";
+            this.ButtonCalculateHorizon.UseVisualStyleBackColor = true;
+            this.ButtonCalculateHorizon.Click += new System.EventHandler(this.ButtonCalculateHorizon_Click);
             // 
             // ButtonGerman
             // 
@@ -241,6 +260,56 @@
             this.ButtonEnglish.UseVisualStyleBackColor = true;
             this.ButtonEnglish.Click += new System.EventHandler(this.ButtonEnglish_Click);
             // 
+            // MainMenu
+            // 
+            this.MainMenu.MenuItems.AddRange(new System.Windows.Forms.MenuItem[] {
+            this.menuItem1});
+            // 
+            // menuItem1
+            // 
+            this.menuItem1.Index = 0;
+            this.menuItem1.MenuItems.AddRange(new System.Windows.Forms.MenuItem[] {
+            this.MenuBackgroundColour,
+            this.MenuButtonColour,
+            this.MenuLabelColour});
+            resources.ApplyResources(this.menuItem1, "menuItem1");
+            // 
+            // MenuBackgroundColour
+            // 
+            this.MenuBackgroundColour.Index = 0;
+            this.MenuBackgroundColour.MenuItems.AddRange(new System.Windows.Forms.MenuItem[] {
+            this.MenuLight,
+            this.MenuDark,
+            this.MenuCustom});
+            resources.ApplyResources(this.MenuBackgroundColour, "MenuBackgroundColour");
+            // 
+            // MenuLight
+            // 
+            this.MenuLight.Index = 0;
+            resources.ApplyResources(this.MenuLight, "MenuLight");
+            this.MenuLight.Click += new System.EventHandler(this.MenuLight_Click);
+            // 
+            // MenuDark
+            // 
+            this.MenuDark.Index = 1;
+            resources.ApplyResources(this.MenuDark, "MenuDark");
+            this.MenuDark.Click += new System.EventHandler(this.MenuDark_Click);
+            // 
+            // MenuCustom
+            // 
+            this.MenuCustom.Index = 2;
+            resources.ApplyResources(this.MenuCustom, "MenuCustom");
+            // 
+            // MenuButtonColour
+            // 
+            this.MenuButtonColour.Index = 1;
+            resources.ApplyResources(this.MenuButtonColour, "MenuButtonColour");
+            // 
+            // MenuLabelColour
+            // 
+            this.MenuLabelColour.Index = 2;
+            resources.ApplyResources(this.MenuLabelColour, "MenuLabelColour");
+            // 
             // ClientForm
             // 
             resources.ApplyResources(this, "$this");
@@ -253,6 +322,9 @@
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.ListViewOutput);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Menu = this.MainMenu;
             this.Name = "ClientForm";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
@@ -262,7 +334,7 @@
             this.groupBox3.PerformLayout();
             this.groupBox4.ResumeLayout(false);
             this.groupBox4.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.InputMass)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -281,22 +353,30 @@
         private System.Windows.Forms.Button ButtonEnglish;
         private System.Windows.Forms.Button ButtonFrench;
         private System.Windows.Forms.Button ButtonGerman;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button ButtonCalculateVelocity;
+        private System.Windows.Forms.Button ButtonCalculateDistance;
+        private System.Windows.Forms.Button ButtonCalculateTemperature;
+        private System.Windows.Forms.Button ButtonCalculateHorizon;
+        private System.Windows.Forms.TextBox InputRest;
+        private System.Windows.Forms.TextBox InputObserved;
+        private System.Windows.Forms.TextBox InputParallax;
+        private System.Windows.Forms.TextBox InputCelsius;
+        private System.Windows.Forms.TextBox InputMass2;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown InputMass;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.MainMenu MainMenu;
+        private System.Windows.Forms.MenuItem menuItem1;
+        private System.Windows.Forms.MenuItem MenuBackgroundColour;
+        private System.Windows.Forms.MenuItem MenuLight;
+        private System.Windows.Forms.MenuItem MenuDark;
+        private System.Windows.Forms.MenuItem MenuCustom;
+        private System.Windows.Forms.MenuItem MenuButtonColour;
+        private System.Windows.Forms.MenuItem MenuLabelColour;
     }
 }
 
--- AstroClient/AstroClient/ClientForm.cs
+++ AstroClient/AstroClient/ClientForm.cs
@@ -10,19 +10,66 @@ using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.ServiceModel;
+using System.Diagnostics;
 
 namespace AstroClient
 {
     public partial class ClientForm : Form
     {
+        // Create a channel reference
+        IAstroContract channel;
+
         public ClientForm()
         {
             InitializeComponent();
 
+            //Initialise the service model channel
             string address = "net.pipe://localhost/pipespacemaths";
             NetNamedPipeBinding binding = new NetNamedPipeBinding(NetNamedPipeSecurityMode.None);
             EndpointAddress ep = new EndpointAddress(address);
-            IAstroContract channel = ChannelFactory<IAstroContract>.CreateChannel(binding, ep);
+            channel = ChannelFactory<IAstroContract>.CreateChannel(binding, ep);
+        }
+
+        private void Display(string[] data)
+        {
+            ListViewItem lvi = new ListViewItem(data[0]);
+            lvi.SubItems.Add(data[1]);
+            lvi.SubItems.Add(data[2]);
+            lvi.SubItems.Add(data[3]);
+            ListViewOutput.Items.Add(lvi);
+        }
+
+        private void ButtonCalculateVelocity_Click(object sender, EventArgs e)
+        {
+            double velocity = channel.StarVelocity(double.Parse(InputObserved.Text), double.Parse(InputRest.Text));
+
+            //string[] data = new string[] { "velocity", "distance", "kelvin", "horizon" };
+            string[] data = new string[] { velocity.ToString("0.00 m/s"), "", "", "" };
+            Display(data);
+        }
+
+        private void ButtonCalculateDistance_Click(object sender, EventArgs e)
+        {
+            double starDistance = channel.StarDistance(double.Parse(InputParallax.Text));
+
+            string[] data = new string[] { "", starDistance.ToString(), "", "" };
+            Display(data);
+        }
+
+        private void ButtonCalculateTemperature_Click(object sender, EventArgs e)
+        {
+            double temperature = channel.TemperatureInKelvin(double.Parse(InputCelsius.Text));
+
+            string[] data = new string[] { "", "", temperature.ToString(), "" };
+            Display(data);
+        }
+
+        private void ButtonCalculateHorizon_Click(object sender, EventArgs e)
+        {
+            double eventHorizon = channel.EventHorizon(double.Parse(InputMass.Text));
+
+            string[] data = new string[] { "", "", "", eventHorizon.ToString() };
+            Display(data);
         }
 
         private void ButtonEnglish_Click(object sender, EventArgs e)
@@ -41,6 +88,18 @@ namespace AstroClient
             ChangeLanguage("German");
         }
 
+        private void TextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != '.')) 
+            {
+                e.Handled = true;
+            }
+            if ((e.KeyChar == '.') && ((sender as TextBox).Text.IndexOf('.') > -1))
+            {
+                e.Handled = true;
+            }
+        }
+
         private void ChangeLanguage(string language)
         {
             switch (language)
@@ -50,7 +109,7 @@ namespace AstroClient
                     ButtonEnglish.Enabled = false;
                     ButtonFrench.Enabled = true;
                     ButtonGerman.Enabled = true;
-                    //this.BackgroundImage = Properties.Resources.Flag_of_US;
+                    this.BackgroundImage = Properties.Resources.flag_english;
 
                     break;
                 case "French":
@@ -58,7 +117,7 @@ namespace AstroClient
                     ButtonEnglish.Enabled = true;
                     ButtonFrench.Enabled = false;
                     ButtonGerman.Enabled = true;
-                    //this.BackgroundImage = Properties.Resources.Flag_of_US;
+                    this.BackgroundImage = Properties.Resources.flag_french;
 
                     break;
                 case "German":
@@ -66,10 +125,20 @@ namespace AstroClient
                     ButtonEnglish.Enabled = true;
                     ButtonFrench.Enabled = true;
                     ButtonGerman.Enabled = false;
-                    //this.BackgroundImage = Properties.Resources.Flag_of_US;
+                    this.BackgroundImage = Properties.Resources.flag_german;
 
                     break;
             }
+        }
+
+        private void MenuLight_Click(object sender, EventArgs e)
+        {
+            BackColor = SystemColors.Control;
+        }
+
+        private void MenuDark_Click(object sender, EventArgs e)
+        {
+            BackColor = SystemColors.InactiveCaption;
         }
     }
 }
